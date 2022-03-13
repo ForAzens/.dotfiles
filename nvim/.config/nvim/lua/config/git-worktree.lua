a2d9be756eb1c9dcbf79211fbb30dc2a1c3c5ab1
Daniel Castillo <azenis.ofc@gmail.com>
2022-03-14 00:05:03 +0100

    More changes in nvim config

--- nvim/.config/nvim/lua/config/git-worktree.lua
+++ nvim/.config/nvim/lua/config/git-worktree.lua
@@ -1,8 +1,10 @@
 require("git-worktree").setup({})
 
-require("git-worktree").on_tree_change(function(op, metadata)
+local Worktree = require("git-worktree")
+
+Worktree.on_tree_change(function(op, metadata)
     print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
     if op == Worktree.Operations.Switch then end
 end)
 
-require("telescope").extensions.git_worktree.create_git_worktree()
+require("telescope").load_extension("git_worktree")
--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,5 +1,5 @@
 require("telescope").setup {
-    defaults = {file_ignore_patterns = {".git", "node_modules"}}
+    defaults = {file_ignore_patterns = {"^.git/", "^node_modules/"}}
 }
 
 require("telescope").load_extension("fzf")
