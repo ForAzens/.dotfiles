f170cb0a9e56f0d83c8b193c8f00dc97c90135cc
Daniel Castillo <azenis.ofc@gmail.com>
2022-04-07 14:34:14 +0200

    Main modules awesome

---
+++ awesome/.config/awesome/main/menu.lua
@@ -0,0 +1,46 @@
+local awful = require("awful")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
+
+local beautiful = require("beautiful")
+
+-- Load Debian menu entries
+local debian = require("debian.menu")
+local has_fdo, freedesktop = pcall(require, "freedesktop")
+
+local M = {} -- menu
+local _M = {} -- module
+
+local terminal = RC.vars.terminal
+
+local editor = os.getenv("EDITOR") or "editor"
+local editor_cmd = terminal .. " -e " .. editor
+
+M.awesome = {
+    {
+        "hotkeys",
+        function() hotkeys_popup.show_help(nil, awful.screen.focused()) end
+    }, {"manual", terminal .. " -e man awesome"},
+    {"edit config", editor_cmd .. " " .. awesome.conffile},
+    {"restart", awesome.restart}, {"quit", function() awesome.quit() end}
+}
+
+function _M.get()
+    local menu_awesome = {"awesome", M.awesome, beautiful.awesome_subicon}
+    local menu_terminal = {"open terminal", terminal}
+
+    local main_menu = {}
+
+    if has_fdo then
+        main_menu = freedesktop.menu.build({
+            before = {menu_awesome},
+            after = {menu_terminal}
+        })
+    else
+        main_menu = awful.menu({items = {menu_awesome, menu_terminal}})
+    end
+
+    return main_menu
+end
+
+---@diagnostic disable-next-line: redundant-parameter
+return setmetatable({}, {__call = function(_, ...) return _M.get(...) end})
---
+++ awesome/.config/awesome/main/tags.lua
@@ -0,0 +1,16 @@
+local awful = require("awful")
+
+local _M = {}
+
+function _M.get()
+    tags = {}
+
+    awful.screen.connect_for_each_screen(function(s)
+        tags[s] = awful.tag({
+            "1 -- Term", "2", "3", "4", "5", "6", "7", "8", "9"
+        }, s, RC.layouts[1])
+    end)
+end
+
+---@diagnostic disable-next-line: redundant-parameter
+return setmetatable({}, {__call = function(_, ...) return _M.get(...) end})
