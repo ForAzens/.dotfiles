07ebeae6c0978f9bb3930b954c7b869800374a25
Daniel Castillo <azenis.ofc@gmail.com>
2022-08-01 10:29:04 +0200

    Fix go to definition in react open qflist with react.d.ts

--- nvim/.config/nvim/lua/lsp/ts_server.lua
+++ nvim/.config/nvim/lua/lsp/ts_server.lua
@@ -2,22 +2,59 @@ local nvim_lsp = require('lspconfig')
 local coq = require('coq')
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
-    -- Mappings
-    local opts = {noremap = true, silent = true}
+  -- Mappings
+  local opts = { noremap = true, silent = true }
 
-    -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    --
+  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  --
 
-    -- Disable tsserver formatting
-    client.resolved_capabilities.document_formatting = false
+  -- Disable tsserver formatting
+  client.resolved_capabilities.document_formatting = false
 
 end
 
+local tsHandlers = {
+  ["textDocument/definition"] = function(_, result, params)
+    local util = require("vim.lsp.util")
+    if result == nil or vim.tbl_isempty(result) then
+      -- local _ = vim.lsp.log.info() and vim.lsp.log.info(params.method, "No location found")
+      return nil
+    end
+
+    if vim.tbl_islist(result) then
+      -- this is opens a buffer to that result
+      -- you could loop the result and choose what you want
+      util.jump_to_location(result[1], 'utf-8')
+
+      if #result > 1 then
+        local isReactDTs = false
+        ---@diagnostic disable-next-line: unused-local
+        for key, value in pairs(result) do
+          if string.match(value.uri, "react/index.d.ts") then
+            isReactDTs = true
+            break
+          end
+        end
+        if not isReactDTs then
+          -- this sets the value for the quickfix list
+          util.set_qflist(util.locations_to_items(result))
+          -- this opens the quickfix window
+          vim.api.nvim_command("copen")
+          vim.api.nvim_command("wincmd p")
+        end
+      end
+    else
+      util.jump_to_location(result, 'utf-8')
+    end
+  end,
+}
+
 nvim_lsp.tsserver.setup(coq.lsp_ensure_capabilities({
   on_attach = on_attach,
+  handlers = tsHandlers,
 }))
