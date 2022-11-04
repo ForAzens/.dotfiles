f25d1c7a054c76a581626554e8e7046570a10b9e
Daniel Castillo <azenis.ofc@gmail.com>
2022-11-04 10:00:01 +0100

    Fix format deprecated command in autocmd.lua

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,8 +1,8 @@
 -- Autoformat lua files
-vim.cmd 'autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)'
+vim.cmd 'autocmd BufWritePre *.lua lua vim.lsp.buf.format {async = true}'
 
 -- Autoformat tsx files
-vim.cmd 'autocmd BufWritePre *.tsx lua vim.lsp.buf.formatting_sync(nil, 100)'
+vim.cmd 'autocmd BufWritePre *.tsx lua vim.lsp.buf.format {async = true}'
 
 -- Autoformat jsx files
-vim.cmd 'autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync(nil, 100)'
+vim.cmd 'autocmd BufWritePre *.jsx lua vim.lsp.buf.format {async = true}'
