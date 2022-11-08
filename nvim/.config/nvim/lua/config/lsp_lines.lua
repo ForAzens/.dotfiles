f528fa3d33e2bd4a9dd309d7b76723182a23b329
Daniel Castillo <azenis.ofc@gmail.com>
2022-11-08 16:34:16 +0100

    Remove unused config plugins

--- nvim/.config/nvim/lua/config/lsp_lines.lua
+++
@@ -1,9 +0,0 @@
-require("lsp_lines").setup()
-
--- Show warnings only in current line
-vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
-
--- Disable virtual_text since it's redundant due to lsp_lines.
-vim.diagnostic.config({
-  virtual_text = false,
-})
